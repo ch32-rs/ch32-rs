169968f918b9952bb9a45461735f2989db0e972f
Andelf <andelf@gmail.com>
2022-11-13 23:51:13 +0800

    feat: script and config file for generate crates

---
+++ scripts/generate.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -ex
+
+./scripts/makecrates.py devices
+
+
+svd patch devices/ch32v30x.yaml
+xmllint --schema svd/cmsis-svd.xsd --noout svd/fixed/ch32v3.svd.patched
+
+mkdir -p ch32v3/src/ch32v30x
+svd2rust -m --target riscv -o ch32v3/src/ch32v30x -g --strict --pascal_enum_values --max_cluster_size -i svd/fixed/ch32v3.svd.patched
+
+mv ch32v3/src/ch32v30x/generic.rs ch32v3/src/
+rm ch32v3/src/ch32v30x/build.rs
+
+rustfmt --config-path="rustfmt.toml" ch32v3/src/ch32v30x/mod.rs
+
+
+
+svd patch devices/ch32v20x.yaml
+xmllint --schema svd/cmsis-svd.xsd --noout svd/fixed/ch32v2.svd.patched
+
+mkdir -p ch32v2/src/ch32v20x
+svd2rust -m --target riscv -g --strict --pascal_enum_values --max_cluster_size \
+    -o ch32v2/src/ch32v20x -i svd/fixed/ch32v2.svd.patched
+
+mv ch32v2/src/ch32v20x/generic.rs ch32v2/src/
+rm ch32v2/src/ch32v20x/build.rs
+
+rustfmt --config-path="rustfmt.toml" ch32v2/src/ch32v20x/mod.rs
+
+
+
+svd patch devices/ch32v103.yaml
+xmllint --schema svd/cmsis-svd.xsd --noout svd/fixed/ch32v1.svd.patched
+
+mkdir -p ch32v1/src/ch32v103
+svd2rust -m --target riscv -g --strict --pascal_enum_values --max_cluster_size \
+    -o ch32v1/src/ch32v103 -i svd/fixed/ch32v1.svd.patched
+
+mv ch32v1/src/ch32v103/generic.rs ch32v1/src/
+rm ch32v1/src/ch32v103/build.rs
+
+rustfmt --config-path="rustfmt.toml" ch32v1/src/ch32v103/mod.rs
---
+++ scripts/update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ex
+
+rm -rv ../ch32-rs-nightlies/ch32v*
+
+mv ch32v* ../ch32-rs-nightlies
